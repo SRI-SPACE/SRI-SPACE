a3fccf33609afd9546804972f98443d3dc8c0d26
SRI-SPACE <ramsri17941@gmail.com>
2025-05-18 16:12:09 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+public class Reverse {
+    public static void main(String[] args) {
+        
+        int i=876;
+
+        while(i>0){
+          System.out.println(i);
+          i--;  
+        }
+    }
+    
+}
