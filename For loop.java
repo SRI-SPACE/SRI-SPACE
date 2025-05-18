2b4b4034997b6cb69c8379e9503ee13cafbdd81a
SRI-SPACE <ramsri17941@gmail.com>
2025-05-18 15:30:05 +0530

    Add files via upload

---
+++ For loop.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("For Loop:");
+        for( int i=10; i<=50; i++){
+            System.out.println(i);
+
+        }
+    }
+}
