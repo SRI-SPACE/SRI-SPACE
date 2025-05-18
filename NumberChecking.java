92a021cbb83ed7b92ad2c00e16a205719da22826
SRI-SPACE <ramsri17941@gmail.com>
2025-05-18 16:24:22 +0530

    Add files via upload

---
+++ NumberChecking.java
@@ -0,0 +1,23 @@
+
+    import java.util.Scanner;
+
+public class NumberChecking {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } 
+    }
+}
+    
+
+
+    
+
