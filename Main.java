bc9bf01b6202771254f84b4f7a88bfcf7e27ecde
SRI-SPACE <ramsri17941@gmail.com>
2025-05-24 17:38:37 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
-
 public class Main {
+    private String name;
+    private int age;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    
+    public Main() {
+        this.age = 18;
+    }
 
-        System.out.print("Enter the purchase amount: ");
-        double purchaseAmount = scanner.nextDouble();
+    
+    public Main(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-        double finalAmount;
-        if (purchaseAmount < 500) {
-            finalAmount = purchaseAmount;
-        } else if (purchaseAmount >= 500 && purchaseAmount <= 1000) {
-            finalAmount = purchaseAmount * 0.9; 
-        } else {
-            finalAmount = purchaseAmount * 0.8; 
-        }
+   
+    public void displayPerson() {
+        System.out.println("Name: " + this.name + ", Age: " + this.age);
+    }
 
-        System.out.println("The final payable amount is: " + finalAmount);
-        scanner.close();
+    public static void main(String[] args){
+        Main person1 = new Main("sri",18); 
+         
+
+        person1.displayPerson();
+        
     }
 }
-
